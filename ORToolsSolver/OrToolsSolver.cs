13b8a7e53e6e3fa17b57e42ccfd340ffbdf34d3e
FLOo89 <florian.eisenbarthvtt@gmail.com>
2020-01-26 11:30:09 +0100

    Ajout de commentaires

--- ORToolsSolver/OrToolsSolver.cs
+++ ORToolsSolver/OrToolsSolver.cs
@@ -12,7 +12,8 @@ namespace ORToolsSolver
         
 
         public Sudoku Solve(Sudoku s)
-        {
+        {   
+            // création du modèle
             CpModel model = new CpModel();
 
             IntVar[][] tab_s = new IntVar[9][];
@@ -20,7 +21,9 @@ namespace ORToolsSolver
             {
                 tab_s[i] = new IntVar[9];
             }
-
+            
+            // création des variables OR-Tools. 
+            //chaque case du sudoku est un Int compris entre 1 et 9 
             for(int i = 0; i<tab_s.Length; i++)
             {
                 for(int j = 0; j<tab_s[i].Length; j++)
@@ -29,12 +32,14 @@ namespace ORToolsSolver
                 }
             }
 
-            //constraints all differents on rows 
+             //on ajoute les contraintes du sudoku 
+            
+            // chaque case doit être differente sur une ligne 
             for(int i =0; i< tab_s.Length;i++)
             {
                 model.AddAllDifferent(tab_s[i]); 
             }
-            // Constraints all differents on colums 
+             // chaque case doit être différente sur une colonne
             IntVar[] tpm = new IntVar[9];
             for(int j = 0; j<tab_s[0].Length;j++)
             {
@@ -46,7 +51,7 @@ namespace ORToolsSolver
                 Array.Clear(tpm, 0, tpm.Length);
             }
 
-            // Constraint all differents on cells 
+            //chaque cellule de 3x3 doit contenir des cases différentes 
             List<IntVar> ls = new List<IntVar>();
             for(int i =0; i< 7; i+=3)
             {
@@ -65,7 +70,7 @@ namespace ORToolsSolver
                 }
             }
 
-            //initial Value
+            //on set les valeurs initiales du sudoku
             for(int i = 0; i<9; i++)
             {
                 for(int j = 0; j<9;j++)
@@ -77,9 +82,12 @@ namespace ORToolsSolver
                 }
             }
 
-            //creation of the Solver 
+            //Création du solveur OR-Tools  
             CpSolver solver = new CpSolver();
+            
+            //résolution du sudoku
             CpSolverStatus status = solver.Solve(model);
+            
             List<int> lsol = new List<int>();
 
             if (status == CpSolverStatus.Feasible)
